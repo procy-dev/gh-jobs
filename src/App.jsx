59c30bfe339a6cc5d12d60623806352c83fa6ff8
Chris Winkler <procy.dev@gmail.com>
2021-04-12 15:10:10 -0700

    Added cards for job listings with some responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useFetchJobs from './hooks/useFetchJobs';
-import { Container } from '@material-ui/core';
+import { Container } from 'react-bootstrap';
+
+import Job from './Components/Job';
 
 const App = () => {
-    const { jobs, loading, error } = useFetchJobs();
-    console.log('jobs', jobs, loading, error)
+    const [params, setParams] = useState({});
+    const [page, setPage] = useState(1);
+    const { jobs, loading, error } = useFetchJobs(params, page);
+
     return (
         <Container>
             {loading && <h1>Loading...</h1>}
             {error && <h1>Error. Try refreshing</h1>}
-            <h1>{jobs.length}</h1>
+            <h1>{jobs.map((job) => {
+                return <Job key={job.id} job={job} />
+            })}</h1>
         </Container>
     );
 }
